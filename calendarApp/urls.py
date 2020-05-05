3a9e7b68084e2a5f136a8adf2d9d85842e0839da
oskar <oskar.kow123@gmail.com>
2020-05-05 19:33:17 +0200

    Dodanie aplikacji frontendowej

--- calendarApp/urls.py
+++ calendarApp/urls.py
@@ -1,14 +1,10 @@
 from django.urls import path, include
+from django.views.generic.base import TemplateView
 
 from . import views
 
-from rest_framework import routers
-from calendarApp.calendar.users.viewsets.UserViewSet import UserViewSet
-
-router = routers.DefaultRouter()
-router.register(r'users', UserViewSet, basename='AppUser')
-
 urlpatterns = [
-    path('', include(router.urls)),
     path('', views.index, name='index'),
+    path(r'users', views.get_users, name='users'),
+    path(r'^.*', TemplateView.as_view(template_name="home.html"), name="home")
 ]
