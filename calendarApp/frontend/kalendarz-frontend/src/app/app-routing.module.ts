e2042ccb24f7c4b03066bb03d03f68a2030321d6
oskar <oskar.kow123@gmail.com>
2020-05-24 20:01:56 +0200

    Komponent login-form

--- calendarApp/frontend/kalendarz-frontend/src/app/app-routing.module.ts
+++ calendarApp/frontend/kalendarz-frontend/src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { HeaderComponent } from './header/header.component'
+import { LoginFormComponent } from './login-form/login-form.component'
 
-const routes: Routes = [];
+
+const routes: Routes = [
+      { path: 'test', component: HeaderComponent },
+      { path: 'test2', component: LoginFormComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- calendarApp/frontend/kalendarz-frontend/src/app/app.module.ts
+++ calendarApp/frontend/kalendarz-frontend/src/app/app.module.ts
@@ -6,11 +6,13 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { LoginFormComponent } from './login-form/login-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    LoginFormComponent
   ],
   imports: [
     BrowserModule,
